e8c913de18a2510f2077e359db449af709517fc8
Jiri Malak <malak.jiri@gmail.com>
2024-10-23 23:54:31 +0200

    update CI build scripts

--- build.bat
+++ build.bat
@@ -1,17 +1,17 @@
 setlocal
 pushd source
 wmake -f ow20.mak -h clean
-@set INCLUDE=.;%WATCOM%\H
+@set INCLUDE=%WATCOM%\H
 wmake -f ow20.mak -h dos32
-@set INCLUDE=.;%WATCOM%\H;%WATCOM%\H\WIN
+@set INCLUDE=%WATCOM%\H;%WATCOM%\H\WIN
 wmake -f ow20.mak -h windows
-@set INCLUDE=.;%WATCOM%\H;%WATCOM%\H\NT
+@set INCLUDE=%WATCOM%\H;%WATCOM%\H\NT
 wmake -f ow20.mak -h winnt
-@set INCLUDE=.;%WATCOM%\H;%WATCOM%\H\NT
+@set INCLUDE=%WATCOM%\H;%WATCOM%\H\NT
 wmake -f ow20.mak -h win32
-@set INCLUDE=.;%WATCOM%\H;%WATCOM%\H\OS2
+@set INCLUDE=%WATCOM%\H;%WATCOM%\H\OS2
 wmake -f ow20.mak -h os2
-rem @set INCLUDE=.;%WATCOM%\LH
+rem @set INCLUDE=%WATCOM%\LH
 rem wmake -f ow20.mak -h curses
 rem wmake -f ow20.mak -h motif
 popd
@@ -22,31 +22,31 @@ setlocal
 pushd design
 @set INCLUDE=%OLD_INCLUDE%
 wmake -f ow20.mak -h clean
-@set INCLUDE=.;%WATCOM%\H;%OZINCPATH%
+@set INCLUDE=%WATCOM%\H;%OZINCPATH%
 wmake -f ow20.mak -h make_dos32_modules
-@set INCLUDE=.;%WATCOM%\H;%WATCOM%\H\WIN;%OZINCPATH%
+@set INCLUDE=%WATCOM%\H;%WATCOM%\H\WIN;%OZINCPATH%
 wmake -f ow20.mak -h make_windows_modules
-@set INCLUDE=.;%WATCOM%\H;%WATCOM%\H\NT;%OZINCPATH%
+@set INCLUDE=%WATCOM%\H;%WATCOM%\H\NT;%OZINCPATH%
 wmake -f ow20.mak -h make_winnt_modules
-@set INCLUDE=.;%WATCOM%\H;%WATCOM%\H\NT;%OZINCPATH%
+@set INCLUDE=%WATCOM%\H;%WATCOM%\H\NT;%OZINCPATH%
 wmake -f ow20.mak -h make_win32_modules
-@set INCLUDE=.;%WATCOM%\H;%WATCOM%\H\OS2;%OZINCPATH%
+@set INCLUDE=%WATCOM%\H;%WATCOM%\H\OS2;%OZINCPATH%
 wmake -f ow20.mak -h make_os2_modules
-rem @set INCLUDE=.;%WATCOM%\LH;%OZINCPATH%
+rem @set INCLUDE=%WATCOM%\LH;%OZINCPATH%
 rem wmake -f ow20.mak -h make_curses_modules
 rem wmake -f ow20.mak -h make_motif_modules
 rem
-@set INCLUDE=.;%WATCOM%\H;%OZINCPATH%
+@set INCLUDE=%WATCOM%\H;%OZINCPATH%
 wmake -f ow20.mak -h dos32
-@set INCLUDE=.;%WATCOM%\H;%WATCOM%\H\WIN;%OZINCPATH%
+@set INCLUDE=%WATCOM%\H;%WATCOM%\H\WIN;%OZINCPATH%
 wmake -f ow20.mak -h windows
-@set INCLUDE=.;%WATCOM%\H;%WATCOM%\H\NT;%OZINCPATH%
+@set INCLUDE=%WATCOM%\H;%WATCOM%\H\NT;%OZINCPATH%
 wmake -f ow20.mak -h winnt
-@set INCLUDE=.;%WATCOM%\H;%WATCOM%\H\NT;%OZINCPATH%
+@set INCLUDE=%WATCOM%\H;%WATCOM%\H\NT;%OZINCPATH%
 wmake -f ow20.mak -h win32
-@set INCLUDE=.;%WATCOM%\H;%WATCOM%\H\OS2;%OZINCPATH%
+@set INCLUDE=%WATCOM%\H;%WATCOM%\H\OS2;%OZINCPATH%
 wmake -f ow20.mak -h os2
-rem @set INCLUDE=.;%WATCOM%\LH;%OZINCPATH%
+rem @set INCLUDE=%WATCOM%\LH;%OZINCPATH%
 rem wmake -f ow20.mak -h curses
 rem wmake -f ow20.mak -h motif
 popd
